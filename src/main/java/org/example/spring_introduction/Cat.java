97b5f53a41faafeeebca51d29e273faf94febee7
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2025-02-10 13:43:16 +0100

    @Component annotation introduced

--- src/main/java/org/example/spring_introduction/Cat.java
+++ src/main/java/org/example/spring_introduction/Cat.java
@@ -1,5 +1,8 @@
 package org.example.spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component("catBean") //if id is not specified, default one is "cat"
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
