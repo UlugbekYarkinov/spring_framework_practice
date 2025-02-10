24014e0a566241ec614f163bb9e168f716c09668
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2025-02-10 13:57:40 +0100

    autowiring of constructors

--- src/main/java/org/example/spring_introduction/Person.java
+++ src/main/java/org/example/spring_introduction/Person.java
@@ -1,17 +1,23 @@
 package org.example.spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
     private int age;
 
+    @Autowired //If we have only one constructor, we can omit @Autowired annotation
     public Person(Pet pet) {
         System.out.println("Person bean is created");
         this.pet = pet;
     }
-    public Person() {
-        System.out.println("Person bean is created");
-    }
+
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     public String getSurname() {
         return surname;
--- src/main/java/org/example/spring_introduction/Test1.java
+++
@@ -1,8 +0,0 @@
-package org.example.spring_introduction;
-
-public class Test1 {
-    public static void main(String[] args) {
-        Pet pet = new Dog();
-        pet.say();
-    }
-}
