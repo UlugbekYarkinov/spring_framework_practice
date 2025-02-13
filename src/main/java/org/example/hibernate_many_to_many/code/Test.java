24440959d09b44e92d83db98d2a90f9208d48c1c
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2025-02-14 00:05:16 +0100

    many-to-many example INSERTING with child

--- src/main/java/org/example/hibernate_many_to_many/code/Test.java
+++ src/main/java/org/example/hibernate_many_to_many/code/Test.java
@@ -13,21 +13,38 @@ public class Test {
                 .addAnnotatedClass(Child.class)
                 .addAnnotatedClass(Section.class)
                 .buildSessionFactory()) {
+//            Session session = factory.getCurrentSession();
+//
+//            Section section1 = new Section("Football");
+//            Child child1 = new Child("Ulugbek", 5);
+//            Child child2 = new Child("Masha", 7);
+//            Child child3 = new Child("Vera", 10);
+//
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child2);
+//            section1.addChildToSection(child3);
+//
+//
+//            session.beginTransaction();
+//
+//            session.persist(section1);
+//
+//            session.getTransaction().commit();
+
             Session session = factory.getCurrentSession();
 
-            Section section1 = new Section("Football");
-            Child child1 = new Child("Ulugbek", 5);
-            Child child2 = new Child("Masha", 7);
-            Child child3 = new Child("Vera", 10);
+            Section section1 = new Section("Volleyball");
+            Section section2 = new Section("Chess");
+            Section section3 = new Section("Math");
+            Child child1 = new Child("Igor", 5);
 
-            section1.addChildToSection(child1);
-            section1.addChildToSection(child2);
-            section1.addChildToSection(child3);
-
+            child1.addSectionToChild(section1);
+            child1.addSectionToChild(section2);
+            child1.addSectionToChild(section3);
 
             session.beginTransaction();
 
-            session.persist(section1);
+            session.persist(child1);
 
             session.getTransaction().commit();
 
